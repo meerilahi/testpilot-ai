6ceb41a61c2b028c9dc4c0184207aa50d3e74352
meerilahi <meerilahidilawar@gmail.com>
2025-05-17 21:02:53 +0500

    update

--- app/config.py
+++
@@ -1,7 +0,0 @@
-# app/config.py
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL")
--- app/db/database.py
+++
@@ -1,8 +0,0 @@
-# app/db/database.py
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-from app.config import DATABASE_URL
-
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
